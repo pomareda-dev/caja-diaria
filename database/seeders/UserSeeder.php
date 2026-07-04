734ec220f50bf3d244b50dad8cd590cce3d9ab22
Alexander Pomareda <marlomp@hotmail.com>
2026-07-04 15:45:36 -0500

    feat: add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Cread admin user
+        $user = new User;
+        $user->name = 'admin';
+        $user->email = 'admin@example.com';
+        $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+        $user->save();
+    }
+}
