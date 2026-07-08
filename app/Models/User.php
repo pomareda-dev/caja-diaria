ea8286f77efc420ff8d2cc4ce89f36682adfe7d8
Lauc1an <marlomp@hotmail.com>
2026-07-07 22:38:35 -0500

    feat: user settings json column
    Adds nullable JSON 'settings' column to users, casts as array in the User model, and exposes it via the TS User type.

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,10 +22,11 @@ use Illuminate\Support\Carbon;
  * @property string|null $two_factor_recovery_codes
  * @property Carbon|null $two_factor_confirmed_at
  * @property string|null $remember_token
+ * @property array|null $settings
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'settings'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -42,6 +43,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'settings' => 'array',
         ];
     }
 
