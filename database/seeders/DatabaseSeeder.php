cbe49ceb1d869a709d027c57641c085bf71c0359
Alexander Pomareda <marlomp@hotmail.com>
2026-07-21 00:22:49 -0500

    fix: update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([CajaDiariaDemoSeeder::class]);
+        $this->call([
+            UserSeeder::class,
+            CajaDiariaDemoSeeder::class,
+        ]);
     }
 }
